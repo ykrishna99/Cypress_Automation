7bc0a383f58676e79084a09d2d0ec4c6cb5927ae
Krishna99 <72746140+ykrishna99@users.noreply.github.com>
2022-05-06 07:59:14 +0530

    initial commit

---
+++ cypress/integration/PracticePrograms/ngx-admin-home.js
@@ -0,0 +1,45 @@
+/// <reference types='Cypress'/>
+
+describe('ngx-admin suite', function () {
+
+    const url = 'http://localhost:4200/pages'
+    const menus = ['Layout', 'Forms', 'Model & Overlays', 'Extra Components', 'Tables & Data', 'Auth']
+    const themesColors = {
+        "Light":"rgb(255, 255, 255)",
+        "Dark":"rgb(34, 43, 69)",
+        "Cosmic":"rgb(50, 50, 89)",
+        "Corporate":"rgb(255, 255, 255)",
+    }
+
+    it('ngx-admin menus', function (){
+        cy.visit(url)
+        cy.title().should('include', 'ngx-admin')
+        cy.get('.logo').should('have.text', 'ngx-admin')
+        cy.get('span.menu-title').each( menuItems => {
+            const actMenu = menuItems.text().trim()   
+            for (let index = 0; index < menus.length; index++) {
+                const element = menus[index];
+                if (element == actMenu) {
+                    // cy.log("Expected " + element + ' matching to actual '  + actMenu)
+                    expect(element).equal(actMenu)
+                }
+                
+            }
+        })
+    })
+    
+    it('ngx-themes', function () {
+        cy.visit(url)
+        cy.get('nb-select').click()
+        cy.get('.options-list nb-option').each( (themes, index) =>{            
+            const themeName = themes.text().trim()
+            cy.wrap(themes).click()
+            cy.get('nb-select').should('contain', themeName)
+            cy.get('nb-layout-header nav').should('have.css', 'background-color', themesColors[themeName])
+            if (index < 3){
+                cy.get('nb-select').click()
+            }            
+        })
+    })
+
+});
